20f3bc20d803b480ec8d4100138895133d009f04
Mordokkai <s.guy09@laposte.net>
2015-10-10 14:28:32 +0200

    début des tests des logiciels

---
+++ test/idct.c
@@ -0,0 +1,34 @@
+
+#include <augh.h>
+
+// FIFO channels for input and output
+// Accesses to these variable will be replaced by AUGH by FIFO handshakes
+uint8_t stdin;
+uint8_t  stdout;
+
+// Arrays used for input and output
+uint8_t input[1];
+uint8_t output[1];
+
+void bidon(){
+	output[0] = (input[0]+input[0])*input[0];
+	output[0] = output[0]*input[0]+input[0]-2*input[0];
+}
+
+// The top-level function for AUGH
+void augh_main () {
+
+	do {
+
+		fifo_read(stdin, &input[0]);
+
+		bidon();
+
+		fifo_write(stdout, &output[0]);
+
+	}while(1);
+
+}
+
+
+
