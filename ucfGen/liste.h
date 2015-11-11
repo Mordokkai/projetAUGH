80c179dce1d5a350b4c39d33358d57ade52446f4
Mordokkai <sylvain.guy@phelma.grenoble-inp.fr>
2015-11-11 18:42:41 +0100

    ajout commentaires

--- ucfGen/liste.h
+++ ucfGen/liste.h
@@ -3,18 +3,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
+// Noeud d'une liste
 struct element{
     char* nom;
     struct element* suiv;
 };
 typedef struct element element;
+
+//Liste
 typedef struct element* Liste;
 
+//Créer un noeud
 element* creerElem();
+
 Liste ajouterEnTete(Liste liste, char* nom);
+
 Liste ajouterEnFin(Liste liste, char* nom);
+
 void afficherListe(Liste liste);
+
 int estVide(Liste liste);
 #endif
--- ucfGen/paths.h
+++ ucfGen/paths.h
@@ -1,31 +1,37 @@
 #ifndef PATHS_H_INCLUDED
 #define PATHS_H_INCLUDED
 #include "liste.h"
+
+//Structure de début ou fin de falsepath
 struct Timegroup{
-    char* nom_tg;   //nom char*
-    Liste comp;     //liste de composants
+    char* nom_tg;   //nom
+    Liste comp;     //liste des composants
 };
 typedef struct Timegroup Timegroup;
 
+//Structure des fils du falsepath entre le début et la fin
 struct Nets{
-    char* nom_nets;  //nom char*
-    Liste nets;
+    char* nom_nets;     //nom
+    Liste nets;         //liste des fils
 };
 typedef struct Nets Nets;
 
+//Structure réprésentant le faux chemin
 struct Falsepath{
-    char* nom_fp;
-    Timegroup* tg_in;    //from timegroup
-    Timegroup* tg_out;   //to timegroup
-    Nets* tg_nets;        //through Nets
+    char* nom_fp;           //nom du faux chemin
+    Timegroup* tg_in;       //Timegroup du début
+    Timegroup* tg_out;      //Timegroup de fin
+    Nets* tg_nets;          //Fils du falsepath
 };
 typedef struct Falsepath Falsepath;
 
 
 Timegroup* creer_Timegroup(char* nom_tg);
 void ajouter_Au_Timegroup(Timegroup* tg, char* comp);
+
 Nets* creer_Nets(char* nom_nets);
 void ajouter_Au_Nets(Nets* nt, char* net);
+
 Falsepath* creer_Falsepath();
 Falsepath* creer_Falsepath_Param(char* nom_fp, char* nom_in, char* nom_out, char* nom_nets);
 #endif // DETECTION_H_INCLUDED
