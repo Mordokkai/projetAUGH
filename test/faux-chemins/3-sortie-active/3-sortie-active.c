0ef7e62dec670bc02cbcd72a66e8d541f235dd9b
Mordokkai <xph3sle509@cimepe37.cime.inpg.fr>
2016-01-25 09:44:19 +0100

    modif fichier test

--- test/faux-chemins/3-sortie-active/3-sortie-active.c
+++ test/faux-chemins/3-sortie-active/3-sortie-active.c
@@ -8,13 +8,12 @@ uint8_t  stdout;
 
 // Arrays used for input and output
 uint8_t input[3];
-uint8_t output[2];
+uint8_t output[1];
 uint8_t output2[1];
 
 void bidon(){
-output[0]=(input[0]+input[1])*input[2];
-output[1]=input[0]-(input[1]*input[2]);
-output2[0]=(input[1]*input[2]);//rajouter du calcul pour faire un faux chemin qui soit critique ?
+output[0]=input[0]-(input[1]*input[2]);
+output2[0]=(input[1]*(input[2]+input[0]));//rajouter du calcul pour faire un faux chemin qui soit critique ?
 }
 
 // The top-level function for AUGH
@@ -28,7 +27,6 @@ void augh_main () {
 		bidon();
 
 		fifo_write(stdout, &output[0]);
-		fifo_write(stdout, &output[1]);
 		fifo_write(stdout, &output2[0]);
 
 	}while(1);
